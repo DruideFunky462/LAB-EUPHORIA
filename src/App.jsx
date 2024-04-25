89a70b81acedd52582e97e10422d01877fa2025e
unknown <oyedokoulwanga@gmail.com>
2024-04-25 11:33:05 +0200

    juste la structure du site sans navbar ni footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+//import Navbar from "./components/Navbar.jsx";
+//import Footer from "./components/Footer.jsx";
+import Home from "./pages/home";
+import Medailles from "./pages/medailles";
+import Legendes from "./pages/legendes";
+import FrenchGlory from "./pages/frenchglory";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Router>
+  
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/frenchglory" element={<FrenchGlory />} />
+          <Route path="/medailles" element={<Medailles />} />
+          <Route path="/legendes" element={<Legendes />} />
+        </Routes>
+
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
+// placer        <Navbar /> entre  Router et Routes  et     <Footer /> en bas
---
+++ src/pages/medailles.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+//import "../styles/medailles.css";
+
+function Medailles() {
+  return (
+    <div  >
+      <p>Medailles</p>
+    </div>
+  );
+  
+}
+
+export default Medailles;
